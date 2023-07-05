26d45934c5a3ab9bc5ec823a4566131b2995adb7
arthurchina92 <arthurchinaa@gmail.com>
2023-07-05 17:58:08 -0600

    Reusable icons implemented in footer

--- Twitter/components/Tweet.tsx
+++ Twitter/components/Tweet.tsx
@@ -1,7 +1,21 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import tweets from "../assets/data/tweets";
 import { TweetType } from "../types";
-const tweet = tweets[0];
+import { Entypo, EvilIcons } from "@expo/vector-icons";
+
+type IconButtonProps = {
+  icon: string;
+  text?: string;
+};
+
+const IconButton = ({ icon, text }: IconButtonProps) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <EvilIcons name={icon} size={22} color="grey" />
+      <Text style={{ fontSize: 12, color: "gray" }}>{text}</Text>
+    </View>
+  );
+};
 
 type TweetProps = {
   tweet: TweetType;
@@ -9,13 +23,33 @@ type TweetProps = {
 
 const Tweet = ({ tweet }: TweetProps) => {
   return (
-    <View style={styles.container}>
-      <Image src={tweet.user.image} style={styles.userImage} />
-      <View style={styles.mainContainer}>
-        <Text style={styles.name}>{tweet.user.name}</Text>
-        <Text style={styles.content}>{tweet.content}</Text>
+    <>
+      <View style={styles.container}>
+        <Image src={tweet.user.image} style={styles.userImage} />
+        <View style={styles.mainContainer}>
+          <View style={styles.userContainer}>
+            <Text style={styles.name}>{tweet.user.name}</Text>
+            <Text style={styles.username}>{tweet.user.username}</Text>
+            <Text style={styles.username}> 2h</Text>
+            <Entypo
+              name="dots-three-horizontal"
+              size={16}
+              color="gray"
+              style={{ marginLeft: "auto" }}
+            />
+          </View>
+          <Text style={styles.content}>{tweet.content}</Text>
+          {tweet.image && <Image src={tweet.image} style={styles.image} />}
+          <View style={styles.footer}>
+            <IconButton icon="comment" text={tweet.numberOfComments} />
+            <IconButton icon="retweet" text={tweet.numberOfRetweets} />
+            <IconButton icon="heart" text={tweet.numberOfLikes} />
+            <IconButton icon="chart" text={tweet.impressions} />
+            <IconButton icon="share-apple" />
+          </View>
+        </View>
       </View>
-    </View>
+    </>
   );
 };
 
@@ -36,12 +70,30 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     flex: 1,
   },
+  userContainer: {
+    flexDirection: "row",
+  },
+  username: {
+    fontWeight: "300",
+    marginLeft: 10,
+  },
   name: {
     fontWeight: "bold",
   },
   content: {
     lineHeight: 20,
     marginTop: 5,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 16 / 9,
+    marginVertical: 10,
+    borderRadius: 15,
+  },
+  footer: {
+    flexDirection: "row",
+    marginVertical: 5,
+    justifyContent: "space-between",
   },
 });
 
